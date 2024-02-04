fffff5f44f6595afd207635ba31faedf87ec6496
ntotev <158792743+ntotev@users.noreply.github.com>
2024-02-04 04:02:05 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+cout << "Hello World!\n";
+
+cout << "Hello, World!\n";
+    cout<<"\tmy name is Aamina!"<<endl;
+    cout<<"something"<<endl;
+    
+    int num1;
+    int num2;
+    cout<<"Please enter a number: ";
+    cin >> num1;
+    cout<<"Please enter another number: ";
+    cin >> num2;
+    cout<< "You have entered " << num1 << " and " << num2<<endl;
+    cout<< num1+num2<<endl;
+string first_name;
+  string last_name;
+  cin >> first_name;
+  cout << + first_name +}
